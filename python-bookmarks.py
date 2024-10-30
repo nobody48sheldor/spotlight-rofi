3bacf52587b645f2ec61ba3c625dd430a619c257
nobody48sheldor <lolmyizi@gmail.com>
2024-10-30 23:16:02 +0100

    update

--- python-bookmarks.py
+++ python-bookmarks.py
@@ -10,6 +10,15 @@ Bookmarks = {
 "info-llg": "https://info-llg.fr/option-mp/?a=cours",
 "youtube": "https://www.youtube.com/",
 "instagram": "https://www.instagram.com/",
+"github": "https://github.com/nobody48sheldor",
+"google calendar": "https://calendar.google.com/calendar/u/0/r?pli=1",
+"moodlecpge": "https://moodlecpge.stanislas.fr/login/index.php",
+"gmail": "https://mail.google.com/mail/u/0/#inbox",
+"cahier-de-prepa": "https://cahier-de-prepa.fr/mp1-janson/",
+"chatgpt": "https://chatgpt.com/",
+"Google Sheets": "https://docs.google.com/spreadsheets/u/0/",
+"e-colle": "https://colles.janson-de-sailly.fr/eleve/action",
+"desmos": "https://www.desmos.com/calculator",
 }
 
 args = sys.argv[1]
